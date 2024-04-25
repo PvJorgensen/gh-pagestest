927ec740fae556cee2ddd3d6617f6086184b9e7a
Patrik_Jørgensen <vadmand806@gmail.com>
2024-04-25 12:44:04 +0200

    Done

--- src/components/CookieBanner/CookieBanner.jsx
+++ src/components/CookieBanner/CookieBanner.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import styles from './cookie.module.scss'
+import ReactGA from "react-ga4";
 
 export const CookieBanner = () => {
     const [consent, setConsent] = useState(false)
